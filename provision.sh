1eb55afb1421ae60bb5a863f0919aa34af3dc9c4
Zakariyya Mughal <zaki.mughal@gmail.com>
2014-01-07 20:12:09 -0600

    add deps for event aggregator

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 apt-get update
-apt-get install -y postgresql sqlite3 libsqlite3-dev build-essential
+apt-get install -y postgresql sqlite3 libsqlite3-dev build-essential libxml2-dev libexpat1-dev
 
 cd /vagrant && su -c ./provision-user.sh vagrant
